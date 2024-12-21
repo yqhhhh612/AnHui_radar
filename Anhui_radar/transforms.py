8e269db17ec44f506f9a461cc4a48066c45884ad
BruceYang <94351796+yqhhhh612@users.noreply.github.com>
2024-12-21 12:05:36 +0800

    Update transforms.py

--- Anhui_radar/transforms.py
+++ Anhui_radar/transforms.py
@@ -114,3 +114,43 @@ def antenna_to_geographic( lon_0, lat_0,h,ranges, azimuths, elevations):
     lon_deg[lon_deg > 180] -= 360.
     lon_deg[lon_deg < -180] += 360.
     return lon_deg, lat_deg
+#求距离
+def getDistance(latA, lonA, latB, lonB):
+    from math import radians,atan,acos,sin,tan,cos
+    ra = 6378140  # radius of equator: meter
+    rb = 6356755  # radius of polar: meter
+    flatten = (ra - rb) / ra  # Partial rate of the earth
+    # change angle to radians
+    radLatA = radians(latA)
+    radLonA = radians(lonA)
+    radLatB = radians(latB)
+    radLonB = radians(lonB)
+
+    pA = atan(rb / ra * tan(radLatA))
+    pB = atan(rb / ra * tan(radLatB))
+    x = acos(sin(pA) * sin(pB) + cos(pA) * cos(pB) * cos(radLonA - radLonB))
+    c1 = (sin(x) - x) * (sin(pA) + sin(pB)) ** 2 / cos(x / 2) ** 2
+    c2 = (sin(x) + x) * (sin(pA) - sin(pB)) ** 2 / sin(x / 2) ** 2
+    dr = flatten / 8 * (c1 - c2)
+    distance = ra * (x + dr)
+    return distance
+def getDegree(latA, lonA, latB, lonB):
+  """
+  Args:
+    point p1(latA, lonA)
+    point p2(latB, lonB)
+  Returns:
+    bearing between the two GPS points,
+    default: the basis of heading direction is north
+  """
+  from math import radians, atan, acos, sin, tan, cos,degrees,atan2
+  radLatA = radians(latA)
+  radLonA = radians(lonA)
+  radLatB = radians(latB)
+  radLonB = radians(lonB)
+  dLon = radLonB - radLonA
+  y = sin(dLon) * cos(radLatB)
+  x = cos(radLatA) * sin(radLatB) - sin(radLatA) * cos(radLatB) * cos(dLon)
+  brng = degrees(atan2(y, x))
+  brng = (brng + 360) % 360
+  return brng
